1ad9bd259a56093b686c64aced7ecbc6b450f284
Connor <chmeadows2@crimson.ua.edu>
2019-05-23 13:56:19 -0500

    Added a balance function

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 public class BST {
     public Node top;
     private ArrayList<Node> trashCan = new ArrayList<Node>();
+    public String io = "";
+    public String po = "";
 
     public BST() {
         top = null;
@@ -39,31 +41,72 @@ public class BST {
         else return;
     }
 
-    public void postOrder(Node t) {
-        if(top == null) return;
-        if(t.left != null)
-            postOrder(t.left);
-        System.out.print(t.val + " ");
-        if(t.right != null)
-            postOrder(t.right);
-    }
-
-    public void inOrder(Node t) {
-        if(top == null) return;
-        System.out.print(t.val + " ");
+    public String inOrder(Node t) {
+        if(top == null) return io;
         if(t.left != null)
             inOrder(t.left);
+        io += (t.val + " ");
         if(t.right != null)
             inOrder(t.right);
+        return io;
+    }
+
+    public String postOrder(Node t) {
+        if(top == null) return po;
+        if(t.left != null)
+            postOrder(t.left);
+        if(t.right != null)
+            postOrder(t.right);
+        po += (t.val + " ");
+        return po;
+    }
+
+    public void balanceTree(Node t) {
+        if(t == null) return;
+        BST newNew = new BST();
+        String inO = inOrder(top);
+        int count = 0;
+        while(inO.length() > 1) {
+            inO = inO.substring(inO.indexOf(" ") + 1);
+            count++;
+        }
+        inO = inOrder(top);
+        int[] temp = new int[count];
+        for(int i = 0; i < count; i++) {
+            temp[i] = Integer.parseInt(inO.substring(0, inO.indexOf(" ")));
+            inO = inO.substring(inO.indexOf(" ") + 1);
+        }
+        int up = (int)(count/2);
+        int down = (int)(count/2 - 1);
+        while (up < count || down > -1) {
+            if(up < count) {
+                System.out.println(temp[up]);
+                newNew.add(temp[up++]);
+            }
+            if(down > -1) {
+                System.out.println(temp[down]);
+                newNew.add(temp[down--]);
+            }
+        }
+        this.trashCan = newNew.trashCan;
+        this.top = newNew.top;
     }
 
     public static void main(String[] args) {
         BST bst = new BST();
+        int addit  = 0;
         for(int i = 0; i < Integer.parseInt(args[0]); i++) {
-            bst.add((int)(Math.random() * 100));
+            addit = (int)(Math.random() * 100);
+            System.out.println(addit);
+            bst.add(addit);
         }
-        bst.postOrder(bst.top);
-        System.out.println();
-        bst.inOrder(bst.top);
+        System.out.println(bst.postOrder(bst.top));
+        System.out.println(bst.inOrder(bst.top));
+        bst.balanceTree(bst.top);
+        bst.io = "";
+        bst.po = "";
+        System.out.println("Balancing...");
+        System.out.println(bst.postOrder(bst.top));
+        System.out.println(bst.inOrder(bst.top));
     }
 }
